156455a38940a91102c12b998f6b385d2c30e95d
golammuktadir29 <golammuktadir29@gmail.com>
2026-06-16 22:27:19 +0600

    lesson 1 & 2

---
+++ lesson1.py
@@ -0,0 +1,14 @@
+# Date : 16/06/2026
+
+# print() => Print's something 
+
+
+print("Hello Sayma!")
+print("How are you Today ?")
+print("Had dinner ?")
+
+# Task : 1 : 
+print("Hello world!!")
+
+
+
---
+++ lesson2.py
@@ -0,0 +1,26 @@
+# Date : 16/06/2026 
+# Topic : Variables 
+
+# x= a+ b ; a= 1, b =2 ; 
+# x = 1+2 = 3 ; 
+
+x = 10 
+print("Line number 8" , "x")
+print("Line number 9" ,"Value of x is :" ,x)
+
+# Task 1: 
+sayma = 15 
+print(sayma)
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+x = 5 
+x= 10 
+x=20
+print(x) ##Note : Always prints the updated value
+
+
+## Topic to cover : Type casting (https://www.w3schools.com/python/python_variables.asp)
+
+
